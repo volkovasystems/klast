fe49289fff3badb9c171cc2a834f9d61d65b6026
volkovasystems <richeve.bebedor@gmail.com>
2017-03-15 06:24:11 +0800

    Finished klast procedure.

---
+++ .install.sh
@@ -0,0 +1,18 @@
+yarn add \
+babel-runtime \
+kley \
+raze \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+kley \
+raze \
+--save \
+--force-latest
---
+++ klast-test.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const klast = require( "./klast.js" );
+
+console.log( klast( { "hello": "world" }, "yeah", [ "ugh" ], { "hello": true } ) );
